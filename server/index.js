ef2fb54e38411b5844539169a1e594dc8337734e
Morteza Araby <morteza.araby@ericsson.com>
2020-04-17 15:45:27 +0200

    update dockerrun.aws & travis

--- server/index.js
+++ server/index.js
@@ -42,7 +42,11 @@ app.get('/', (req, res) => {
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
-  res.send(values.rows);
+  if(values){
+    res.send(values.rows);
+  } else {
+    res.send(null);
+  }
 });
 
 app.get('/values/current', async (req, res) => {
